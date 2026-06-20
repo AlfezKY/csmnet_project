4c42a182ebed44cffc9af210c1e0dff028db3a81
AlfezKY <d1041211025@student.untan.ac.id>
2026-06-20 20:34:36 +0700

    Final Pengeluaran: Storage Cloud

--- resources/views/admin/pengeluaran/index.blade.php
+++ resources/views/admin/pengeluaran/index.blade.php
@@ -260,7 +260,7 @@
                     
                     <td class="px-6 py-4 text-center whitespace-nowrap">
                         @if($out->bukti_bayar)
-                            <a href="{{ asset($out->bukti_bayar) }}" target="_blank" class="inline-flex items-center gap-1 px-2.5 py-1 text-[11px] font-bold text-blue-600 bg-blue-50 border border-blue-100 hover:bg-blue-600 hover:text-white rounded-md transition-all" title="Lihat Lampiran">
+                            <a href="{{ Storage::disk('s3')->url($out->bukti_bayar) }}" target="_blank" class="inline-flex items-center gap-1 px-2.5 py-1 text-[11px] font-bold text-blue-600 bg-blue-50 border border-blue-100 hover:bg-blue-600 hover:text-white rounded-md transition-all" title="Lihat Lampiran">
                                 <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path></svg>
                                 Lihat
                             </a>
@@ -435,7 +435,7 @@
                         <div class="mt-1.5 flex items-center justify-between px-1">
                             <p class="text-[10px] font-medium text-gray-500">*Kosongkan jika tidak ingin mengubah file lama.</p>
                             <template x-if="editData.bukti_bayar">
-                                <a :href="'/' + editData.bukti_bayar" target="_blank" class="text-[10px] font-black tracking-wide text-blue-600 hover:underline">Lihat File Saat Ini</a>
+                                <a :href="'https://rufusthcsnjroevstvlo.supabase.co/storage/v1/object/public/pengeluaran/' + editData.bukti_bayar" target="_blank" class="text-[10px] font-black tracking-wide text-blue-600 hover:underline">Lihat File Saat Ini</a>
                             </template>
                         </div>
                     </div>
